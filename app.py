d8b700508d77f6e92eac838ae7d4b42970f3ac28
vmart45 <116533208+vmart45@users.noreply.github.com>
2025-11-19 13:30:07 -0800

    Adjust height parameter in components.html

--- app.py
+++ app.py
@@ -811,7 +811,7 @@ if page == "Main":
             renderTable();
         </script>
         """
-    components.html(html_table, height=1000, scrolling=True)
+    components.html(html_table, height=900, scrolling=True)
 
 
 
