a0da74360b0a91014c28f953c62da42320a4a2da
vmart45 <116533208+vmart45@users.noreply.github.com>
2025-11-19 14:03:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -844,7 +844,7 @@ if page == "Main":
             renderTable();
         </script>
         """
-    components.html(html_table, height=1100, scrolling=True)
+    components.html(html_table, height=1100, scrolling=False)
 
 
 
