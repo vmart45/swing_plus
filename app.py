d6533c09d3608b64b22e366e3b306b761170be99
vmart45 <116533208+vmart45@users.noreply.github.com>
2025-11-20 11:25:16 -0800

    Increase height of HTML table to 1400

--- app.py
+++ app.py
@@ -854,7 +854,7 @@ if page == "Main":
         </script>
         """
 
-    components.html(html_table, height=1100, scrolling=True)
+    components.html(html_table, height=1400, scrolling=True)
 
 
 
