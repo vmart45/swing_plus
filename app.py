938409a37d6484de2d8664a5bd16c41f54d1b6f2
vmart45 <116533208+vmart45@users.noreply.github.com>
2025-11-24 09:54:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -854,7 +854,7 @@ if page == "Main":
         </script>
         """
 
-    components.html(html_table, height=1500, scrolling=True)
+    components.html(html_table, height=1550, scrolling=True)
 
 
 
